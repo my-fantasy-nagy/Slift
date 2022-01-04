a41b1c9efe7c755a39f8f11974887c5792da8991
Alex Nagy <alexnagy@Alexs-MacBook-Pro.local>
2022-01-03 21:08:35 -0800

    fleshed out slider

--- src/Slider.java
+++ src/Slider.java
@@ -8,18 +8,42 @@ public class Slider {
     double pos;
     int intPos;
     boolean forward;
+    boolean pause;
 
     public Slider(double rate, double max, double min) {
         this.rate = rate;
         this.max = max;
         this.min = min;
         forward = true;
+        pause = false;
         pos = min;
         intPos = (int)pos;
-
     }
 
     public void update(){
 
+        //check direction and move position accordingly
+        if(!pause) {
+            if (forward) {
+                pos += rate;
+            } else {
+                pos -= rate;
+            }
+        }
+
+        if(pos >= max){
+            pos = max;
+        }
+
+        if(pos <= min){
+            pos = min;
+        }
+    }
+
+    public void reverseDirection(){
+        forward = !forward;
+    }
+    public double getPosition(){
+        return pos;
     }
 }
