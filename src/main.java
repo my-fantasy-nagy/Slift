626f4a04c68f80eab5c9a11bd6beb899e16e78c9
Alex Nagy <alexnagy@Alexs-MacBook-Pro.local>
2022-01-03 20:34:02 -0800

    Removed hello world / added main

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class main extends PApplet {
+
+    public static void main(String... args){
+        PApplet.main("main");
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup(){
+        background(0);
+    }
+
+    public void draw(){
+        background(0);
+    }
+}
